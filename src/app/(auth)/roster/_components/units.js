fbb1b6b5be0881a0fdde2f1eb094e9a0318eadb3
Jon Erickson <jon@deschutesdesigngroup.com>
2024-02-07 09:43:50 -0800

    wip

--- src/app/(auth)/roster/_components/units.js
+++ src/app/(auth)/roster/_components/units.js
@@ -1,12 +1,14 @@
 import { Unit } from './unit';
 
 export function Units({ unit }) {
-  const { units } = unit;
+  const { subunits } = unit;
 
   return (
     <>
       <Unit unit={unit} />
-      {units && !!units.length && units.map((subunit) => <Unit key={subunit.id} unit={subunit} />)}
+      {subunits &&
+        !!subunits.length &&
+        subunits.map((subunit) => <Unit key={subunit.id} unit={subunit} />)}
     </>
   );
 }
